60bf7c051a9705eeebdcba20e177d532876ec3f9
analisis1G22015 <alan220193@hotmail.com>
2015-10-19 12:13:01 -0600

    Inicio determinante

--- proyecto2/matrices.js
+++ proyecto2/matrices.js
@@ -79,78 +79,7 @@ if(matArr.length==2){
 }else{//DETERMINANTE
 	console.log("es determinante");
 	if(matriz1.length==matriz1[0].length){
-		//encontrar primer pivote
-		var limite=matriz1.length;
-		var j=0;
-		var determinante=1;
-		for(var i=0;i<limite;i++){
-			if(matriz1[i][0]==1){
-				j=i;
-				//console.log("Uno encontrado en 0,"+j);
-			}
-		}
-		if(j!=0){//CAMBIAR LAS FILAS
-			//console.log("limite "+limite);
-			var matSust=new Array(limite);
-			for(var i=0;i<limite;i++)
-				matSust[i]=new Array(limite);
-			for(var i=limite-1;i>=0;i--){				
-				for(var k=0;k<limite;k++){
-					if(i===j){
-						matSust[0][k]=matriz1[i][k];
-					}else if(i==0){
-						matSust[j][k]=matriz1[i][k];
-					}else{
-						matSust[i][k]=matriz1[i][k];
-					}
-				}
-			}
-			determinante*=-1;			
-			console.log("Fila ya cambiada");
-			console.log(matSust);
-			console.log("Filas "+matSust.length+", columnas "+matSust[0].length);
-			//IR HACIENDO 0 TODO LO DEBAJO DE LA DIAGONAL?
-			//PRIMERO BUSCAR ALGUN 0 ABAJO			
-			var contador=-1;
-			for(var i=0;i<limite;i++){
-				debugger;
-				for(var j=0;j<limite;j++){
-					if(i==j){	//VERIFICAR SI ES UNO, SINO DIVIDIR TODA LA FILA POR SU VALOR Y MULTIPLICAR EL DETERMINANTE
-						contador++;
-						var val=matSust[i][j];
-						console.log("val i=j "+val);
-						if(val!=1){
-							for(var k=0;k<limite;k++){
-								matSust[i][k]=Number(matSust[i][k])/Number(matSust[i][k]);								
-							}
-							determinante*=Number(val);
-						}
-					}else{
-						if(j>0){	//Solo por debajo de la diagonal
-							console.log((j-1)+","+contador);
-							var val=matSust[j-1][contador];							
-							var val2=matSust[j][i];
-							//console.log("val  "+val2);
-							if(val2!=0){
-								for(var k=0;k<limite && j>0;k++){											
-									val=matSust[j-1][k];				
-									console.log("val arriba "+val);									
-									val2=matSust[j][k];									
-									val2=(Number(val2)*(-1))*Number(val);
-									console.log(val2);		
-									matSust[j][k]=Number(matSust[j][k])+val2;
-									console.log("nuevo i,j "+j+", "+k+" - "+matSust[j][k]);		
-								}
-							}
-						}
-					}					
-				}				
-			}
-			console.log("deberia sacar la diagonal 1 y 0 abajo");
-			console.log(matSust);
-		}else{//YA ESTA EN LA FILA 1 O NO HAY NINGUN 1
-
-		}
+		
 	}else{
 		console.log("No es matriz cuadrada");
 	}
