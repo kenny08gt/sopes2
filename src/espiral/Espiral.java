775e33be4144f808a1759b0e6d4ab7d6c5ad069d
analisis1G22015 <alan220193@hotmail.com>
2015-10-23 20:22:58 -0600

    espiral para nodos

--- src/espiral/Espiral.java
+++ src/espiral/Espiral.java
@@ -29,103 +29,100 @@ public class Espiral {
         for (int i = 0; i < args.length; i++) {
             System.out.println(args[i]);
         }
-        String nodo=args[0];
-        String limite=args[1];
-        Espiral e=new Espiral();
-        e.iniciar(Integer.parseInt(nodo),Integer.parseInt(limite));
-    }
-    public void iniciar(int id_nodo, int limite){
-        try {
-            int m=0,cont=0;            
-            m=limite;
-            int N=(int) ((sqrt(m) -1)/2);
-            N=2*N+1;
-            //System.out.println("Filas y columnas: "+N);
-            int [][]matriz=new int[N][N];
-            int x=(int)(N/2);
-            int y=x;
-            int arriba=1,abajo=2,derecha=1,izquierda=2;
-            while(cont<m){
-                //System.out.println("inicio while %d y %d\n",cont,m);
-                int i=0;
-                for(i=0;i<derecha && cont<m && x<N && y<N;i++){
-                    cont++;
-                    
-                    //if(x==N) System.out.println("X igual a N con "+cont);
-                    //if(y==N) System.out.println("Y igual a N con "+cont);
-                    matriz[x][y]=cont;
-                    //System.out.println("en "+x+","+y+", hay: "+cont);
-                    x++;
-                }
-                i=0;
-                derecha+=2;
-                for(i=0;i<arriba && cont<m && x<N && y<N;i++){
-                    cont++;                    
-                    
-                    //if(x==N) System.out.println("X igual a N con "+cont);
-                   // if(y==N) System.out.println("Y igual a N con "+cont);
-                    matriz[x][y]=cont;
-                    // System.out.println("en %d,%d, hay: %d\n",x,y,cont);
-                    //System.out.println("en "+x+","+y+", hay: "+cont);
-                    y--;
-                }
-                i=0;
-                arriba+=2;
-                for(i=0;i<izquierda && cont<m && x<N && y<N;i++){
-                    cont++;
-                    //if(x==N) System.out.println("X igual a N con "+cont);
-                    //if(y==N) System.out.println("Y igual a N con "+cont);
-                    matriz[x][y]=cont;
-                    //System.out.println("en %d,%d, hay: %d\n",x,y,cont);
-                    //System.out.println("en "+x+","+y+", hay: "+cont);
-                    x--;
-                }
-                izquierda+=2;
-                i=0;
-                for(i=0;i<abajo && cont<m && x<N && y<N;i++){
-                    cont++;                                        
-                    //if(x==N) System.out.println("X igual a N con "+cont);
-                    //if(y==N) System.out.println("Y igual a N con "+cont);
-                    matriz[x][y]=cont;
-                    //System.out.println("en "+x+","+y+", hay: "+cont);
-                    y++;
-                }
-                abajo+=2;
-                if(x==N || y==N) break;
-            }
-            graficar(matriz,N);
-        } catch (IOException ex) {
-            Logger.getLogger(Espiral.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        String nodo = args[0];
+        String inicio = args[1];        
+        String fin = args[2];        
+        Espiral e = new Espiral();        
+        e.iniciar(Integer.parseInt(nodo), Integer.parseInt(inicio),Integer.parseInt(fin));
     }
 
+    public void iniciar(int id_nodo, int inicio,int fin) {        
+        int m = 0, cont = inicio;
+        m = inicio-fin;
+        int N = (int) ((sqrt(m) - 1) / 2);
+        N = 2 * N + 1;
+        //System.out.println("Filas y columnas: "+N);
+        int[][] matriz = new int[N][N];
+        int x = (int) (N / 2);
+        int y = x;
+        int arriba = 1, abajo = 2, derecha = 1, izquierda = 2;        
+        while (cont < m) {
+            //System.out.println("inicio while %d y %d\n",cont,m);
+            int i = 0;
+            for (i = 0; i < derecha && cont < m && x < N && y < N; i++) {
+                cont++;
+                matriz[x][y] = cont;
+                x++;
+            }
+            i = 0;
+            derecha += 2;
+            for (i = 0; i < arriba && cont < m && x < N && y < N; i++) {
+                cont++;
+                matriz[x][y] = cont;
+                y--;
+            }
+            i = 0;
+            arriba += 2;
+            for (i = 0; i < izquierda && cont < m && x < N && y < N; i++) {
+                cont++;
+                matriz[x][y] = cont;
+                x--;
+            }
+            izquierda += 2;
+            i = 0;
+            for (i = 0; i < abajo && cont < m && x < N && y < N; i++) {
+                cont++;
+                matriz[x][y] = cont;
+                y++;
+            }
+            abajo += 2;
+            if (x == N || y == N) {
+                break;
+            }
+        }
+        graficar2(matriz, N);
+    }
+    private void graficar2(int[][] matriz, int n) {
+        for (int j = 0; j < n; j++) {            
+            for (int i = 0; i < n; i++) {                
+                if (es_primo(matriz[i][j]) == 1) {
+                    System.out.println("P");                    
+                } else {
+                    System.out.println(matriz[i][j]);                              
+                }
+            }            
+        }
+    }
     private void graficar(int[][] matriz, int n) {
         System.out.println("graficar");
-        PrintWriter writer=null;
+        PrintWriter writer = null;
         try {
-            String nombre="/home/alan/espiral.dot";
+            String nombre = "/home/alan/espiral.dot";
             //    std::cout<<nombre<<std::endl;
-            String pf="";
-            pf="digraph G {abc [shape=none, margin=0, label=< <TABLE BORDER=\"1\" CELLBORDER=\"0\" CELLSPACING=\"0\" CELLPADDING=\"0\" BGCOLOR=\"black\">";
-            int i=0,j=0;
-            for(j=0;j<n;j++){
-                pf+="<tr>";
-                for(i=0;i<n;i++){
+            String pf = "";
+            pf = "digraph G {abc [shape=none, margin=0, label=< <TABLE BORDER=\"1\" CELLBORDER=\"0\" CELLSPACING=\"0\" CELLPADDING=\"0\" BGCOLOR=\"black\">";
+            int i = 0, j = 0;
+            for (j = 0; j < n; j++) {
+                pf += "<tr>";
+                for (i = 0; i < n; i++) {
                     //System.out.println("%d,%d hay %d\n",i,j,matriz[i][j]);                    
-                    
-                    if(es_primo(matriz[i][j])==1){
+
+                    if (es_primo(matriz[i][j]) == 1) {
                         //pf+="<td BGCOLOR=\"lawngreen\">"+matriz[i][j]+"</td>";
-                        pf+="<td width=\"25\" height=\"25\" fixedsize=\"true\"  BGCOLOR=\"lawngreen\"> * </td>";
-                    }else{
+                        pf += "<td width=\"25\" height=\"25\" fixedsize=\"true\"  BGCOLOR=\"lawngreen\"> * </td>";
+                    } else {
                         //pf+="<td>"+matriz[i][j]+"</td>";
-                        pf+="<td width=\"25\" height=\"25\" fixedsize=\"true\" > . </td>";
+                        pf += "<td width=\"25\" height=\"25\" fixedsize=\"true\" > . </td>";
                     }
                 }
-                pf+="</tr>\n";
-            }   /*************************/
-        pf+="</TABLE>>];\n}";
-        writer = new PrintWriter(nombre, "UTF-8");
-        writer.println(pf);
+                pf += "</tr>\n";
+            }
+            /**
+             * **********************
+             */
+            pf += "</TABLE>>];\n}";
+            writer = new PrintWriter(nombre, "UTF-8");
+            writer.println(pf);
             writer.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Espiral.class.getName()).log(Level.SEVERE, null, ex);
@@ -137,18 +134,27 @@ public class Espiral {
     }
 
     private int es_primo(int n) {
-        if(n==1)return 0;
-        int i=0,j=n-1,c=0;
-        for(i=0;i<n;i++){
-            if(j>0 && (n%j)==0) c++;
-            if(c>1) return 0;
+        if (n == 1) {
+            return 0;
+        }
+        int i = 0, j = n - 1, c = 0;
+        for (i = 0; i < n; i++) {
+            if (j > 0 && (n % j) == 0) {
+                c++;
+            }
+            if (c > 1) {
+                return 0;
+            }
             j--;
         }
-        if(c>1) return 0;
-        else return 1;
+        if (c > 1) {
+            return 0;
+        } else {
+            return 1;
+        }
         //1er  j=3 i=0 n=4 c=0
         //2da  j=2 i=1 n=4 c=1
         //3er  j=1 i=2 n=4 c=2
     }
-    
+
 }
